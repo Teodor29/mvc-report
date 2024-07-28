d82a88c81c435a57966a11baefcae2d52628e3a0
Teodor29 <teodor.lien@gmail.com>
2024-07-28 16:50:23 +0200

    4.0.0

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e7ac431508cbe5e557527a0367693772eb9c322e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e7ac431508cbe5e557527a0367693772eb9c322e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.11.8',
+            'version' => '1.11.8.0',
+            'reference' => '6adbd118e6c0515dd2f36b06cde1d6da40f1b8ec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
